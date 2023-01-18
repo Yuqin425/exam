8681e95b88a124abdb3c200fd030992a3a146b9e
Yuqin425 <114483458+Yuqin425@users.noreply.github.com>
2023-01-18 16:33:01 +0800

    Add files via upload

---
+++ api/middleware/auth.go
@@ -0,0 +1,40 @@
+package middlewares
+
+import (
+	"LanShan/api"
+	"LanShan/api/middlewares/jwt"
+	"LanShan/utils"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strings"
+)
+
+// JWTAuthMiddleware 基于JWT的认证中间件
+func JWTAuthMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader == "" {
+			utils.ResponseErrorWithMsg(c, utils.CodeInvalidToken, "请求头缺少Auth Token")
+			c.Abort()
+			return
+		}
+		// 按空格分割
+		parts := strings.SplitN(authHeader, " ", 2)
+		if !(len(parts) == 2 && parts[0] == "Bearer") {
+			utils.ResponseErrorWithMsg(c, utils.CodeInvalidToken, "Token格式不对")
+			c.Abort()
+			return
+		}
+		// parts[1]是获取到的tokenString，我们使用之前定义好的解析JWT的函数来解析它
+		mc, err := jwt.ParseToken(parts[1])
+		if err != nil {
+			fmt.Println(err)
+			utils.ResponseError(c, utils.CodeInvalidToken)
+			c.Abort()
+			return
+		}
+		// 将当前请求的userID信息保存到请求的上下文c上
+		c.Set(api.ContextUserIDKey, mc.UserID)
+		c.Next() // 后续的处理函数可以用过c.Get(ContextUserIDKey)来获取当前请求的用户信息
+	}
+}
